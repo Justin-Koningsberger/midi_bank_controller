e5fee1d1f24b521bfb1b87cad97d64e09798071d
Justin <justin.koningsberger@gmail.com>
2020-03-09 18:27:18 +0100

    Gnome bank logic and tests

--- controller.rb
+++ controller.rb
@@ -10,16 +10,14 @@ class Controller
   end
 
   def process value
-    begin
-      #binding.pry
-      chrome_panel(value)
-    rescue => e
-      puts e.inspect
-      puts e.backtrace.join("\n")
+    if (0..6) === value
+      chrome_bank(value)
+    elsif (7..14) === value
+      gnome_bank(value)
     end
   end
 
-  def chrome_panel value
+  def chrome_bank value
     if value == 0
       puts 'Pages'
       @chrome_state = :pages
@@ -63,7 +61,90 @@ class Controller
         when 5
           xdo_key 'Control_L+F4'
       end
-    end  
+    end
+  end
+
+  GNOME_WINDOWS = [nil, :VirtualBox, :chromium, :terminal, :Firefox, :thunderbird, :sublime, :slack]
+
+  def gnome_bank value
+    value -= 7 # align value with GNOME_WINDOWS
+
+    case value
+    when 0
+      return "Waiting for key up" if @lock.locked?
+      @lock.lock
+      Thread.abort_on_exception = true
+      @thr = Thread.new do |thread|
+        started_at = Time.now.to_f
+        repeat_threshold = 0.99
+        result = ''
+        while @lock.locked?
+          long_press = (Time.now.to_f - started_at) > repeat_threshold
+          sleep 0.2 if long_press
+          if long_press
+            result = "#{activate_window("#{GNOME_WINDOWS[value + 1]}")}"
+          end
+        end
+        if (Time.now.to_f - started_at) <= repeat_threshold
+          result = "#{xdo_key 'Return'}"
+        end
+        result
+      end
+      :started_thread
+    when 1
+      @lock.unlock
+      @thr.value
+    when 2
+      return "Waiting for key up" if @lock.locked?
+      @lock.lock
+      Thread.abort_on_exception = true
+      @thr = Thread.new do |thread|
+        long_press_activate_window(GNOME_WINDOWS[value], GNOME_WINDOWS[value + 1])
+      end
+      :started_thread
+    when 3
+      @lock.unlock
+      @thr.value
+    when 4
+      return "Waiting for key up" if @lock.locked?
+      @lock.lock
+      Thread.abort_on_exception = true
+      @thr = Thread.new do |thread|
+        long_press_activate_window(GNOME_WINDOWS[value], GNOME_WINDOWS[value + 1])
+      end
+      :started_thread
+    when 5
+      @lock.unlock
+      @thr.value
+    when 6
+      return "Waiting for key up" if @lock.locked?
+      @lock.lock
+      Thread.abort_on_exception = true
+      @thr = Thread.new do |thread|
+        long_press_activate_window(GNOME_WINDOWS[value], GNOME_WINDOWS[value + 1])
+      end
+      :started_thread
+    when 7
+      @lock.unlock
+      @thr.value
+    end
+  end
+
+  def long_press_activate_window(window_x, window_y)
+    started_at = Time.now.to_f
+    result = ''
+    repeat_threshold = 0.99
+    while @lock.locked?
+      long_press = (Time.now.to_f - started_at) > repeat_threshold
+      sleep 0.2 if long_press
+      if long_press
+        result = "#{activate_window window_y}"
+      end
+    end
+    if (Time.now.to_f - started_at) <= repeat_threshold
+      result = "#{activate_window window_x}"
+    end
+    result
   end
 
   def long_press(short, long)
@@ -82,6 +163,10 @@ class Controller
       result += ", #{xdo_key long}"
     end
     result
+  end
+
+  def activate_window(program)
+    xdotool("search --onlyvisible --class --classname --name #{program} windowactivate")
   end
 
   def _bank value
@@ -110,7 +195,13 @@ class Controller
   end
 
   def xdotool str
-    `DISPLAY=':0.0' xdotool #{str}`
-    "xdotool #{str}"
+    case @testing
+    when false
+      `DISPLAY=':0.0' xdotool #{str}`
+      "xdotool #{str}"
+    when true
+      puts "xdotool #{str}"
+      "xdotool #{str}"
+    end
   end
 end
--- test_controller.rb
+++ test_controller.rb
@@ -13,45 +13,105 @@ def expect_equal (fixture, result)
   end
 end
 
-controller = Controller.new(testing: true)
-chrome_outputs = []
-chrome_fixtures = [:tabs,
-            :pages,
-            "xdotool key Control_L+Alt_L+r",
-            :started_thread,
-            "result: , xdotool key Page_Up",
-            :started_thread,
-            "result: , xdotool key Page_Down",
-            :tabs,
-            "xdotool key Control_L+Shift_L+Tab",
-            "xdotool key Control_L+Tab",
-            "xdotool key Control_L+F4",
-            :pages,
-            :started_thread,
-            "result: , xdotool key Up, xdotool key Up",
-            :started_thread,
-            "result: , xdotool key Down, xdotool key Down"]
+def test_chrome_bank
+  controller = Controller.new(testing: true)
+  @chrome_outputs = []
+  @chrome_fixtures = [:tabs,
+              :pages,
+              "xdotool key Control_L+Alt_L+r",
+              :started_thread,
+              "result: , xdotool key Page_Up",
+              :started_thread,
+              "result: , xdotool key Page_Down",
+              :tabs,
+              "xdotool key Control_L+Shift_L+Tab",
+              "xdotool key Control_L+Tab",
+              "xdotool key Control_L+F4",
+              :pages,
+              :started_thread,
+              "result: , xdotool key Up, xdotool key Up",
+              :started_thread,
+              "result: , xdotool key Down, xdotool key Down"]
 
-chrome_outputs << controller.process(1)
-chrome_outputs << controller.process(0) # to pages
-chrome_outputs << controller.process(2)
-chrome_outputs << controller.process(3)
-sleep 0.1
-chrome_outputs << controller.process(4)
-chrome_outputs << controller.process(5)
-sleep 0.1
-chrome_outputs << controller.process(6)
-chrome_outputs << controller.process(1) #to tabs
-chrome_outputs << controller.process(2)
-chrome_outputs << controller.process(3)
-chrome_outputs << controller.process(5)
-chrome_outputs << controller.process(0) # to pages
-chrome_outputs << controller.process(3)
-sleep 1.6
-chrome_outputs << controller.process(4)
-chrome_outputs << controller.process(5)
-sleep 1.6
-chrome_outputs << controller.process(6)
+  @chrome_outputs << controller.process(1)
+  @chrome_outputs << controller.process(0) # to pages
+  @chrome_outputs << controller.process(2)
+  @chrome_outputs << controller.process(3)
+  sleep 0.1
+  @chrome_outputs << controller.process(4)
+  @chrome_outputs << controller.process(5)
+  sleep 0.1
+  @chrome_outputs << controller.process(6)
+  @chrome_outputs << controller.process(1) #to tabs
+  @chrome_outputs << controller.process(2)
+  @chrome_outputs << controller.process(3)
+  @chrome_outputs << controller.process(5)
+  @chrome_outputs << controller.process(0) # to pages
+  @chrome_outputs << controller.process(3)
+  sleep 1.6
+  @chrome_outputs << controller.process(4)
+  @chrome_outputs << controller.process(5)
+  sleep 1.6
+  @chrome_outputs << controller.process(6)
+  puts "----------------------------"
+end
+
+def test_gnome_bank
+  controller = Controller.new(testing: true)
+  @gnome_outputs = []
+  @gnome_fixtures = 
+    [:started_thread,
+     "xdotool key Return",
+     :started_thread,
+     "xdotool search --onlyvisible --class --classname --name chromium windowactivate",
+     :started_thread,
+     "xdotool search --onlyvisible --class --classname --name Firefox windowactivate",
+     :started_thread,
+     "xdotool search --onlyvisible --class --classname --name sublime windowactivate",
+     :started_thread,
+     "xdotool search --onlyvisible --class --classname --name VirtualBox windowactivate",
+     :started_thread,
+     "xdotool search --onlyvisible --class --classname --name terminal windowactivate",
+     :started_thread,
+     "xdotool search --onlyvisible --class --classname --name thunderbird windowactivate",
+     :started_thread,
+     "xdotool search --onlyvisible --class --classname --name slack windowactivate"]
+
+  @gnome_outputs << controller.process(7)
+  sleep 0.1
+  @gnome_outputs << controller.process(8)
+  @gnome_outputs << controller.process(9)
+  sleep 0.1
+  @gnome_outputs << controller.process(10)
+  @gnome_outputs << controller.process(11)
+  sleep 0.1
+  @gnome_outputs << controller.process(12)
+  @gnome_outputs << controller.process(13)
+  sleep 0.1
+  @gnome_outputs << controller.process(14) # Short presses
+
+  @gnome_outputs << controller.process(7)
+  sleep 1.0
+  @gnome_outputs << controller.process(8)
+  @gnome_outputs << controller.process(9)
+  sleep 1.0
+  @gnome_outputs << controller.process(10)
+  @gnome_outputs << controller.process(11)
+  sleep 1.0
+  @gnome_outputs << controller.process(12)
+  @gnome_outputs << controller.process(13)
+  sleep 1.0
+  @gnome_outputs << controller.process(14) # long presses
+
+
+  puts "----------------------------"
+end
 
 puts "----------------------------"
-expect_equal(chrome_fixtures, chrome_outputs)
+# test_chrome_bank
+
+# expect_equal(@chrome_fixtures, @chrome_outputs)
+
+test_gnome_bank
+
+expect_equal(@gnome_fixtures, @gnome_outputs)
